c1fc069649d729ae02d35b77883ba4dcc8b73c49
A Likith Reddy <alikithreddy381@gmail.com>
2025-09-17 18:09:07 +0530

    Log out functionality moved to profile page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,24 +1,10 @@
-import 'package:ewallet/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              context.read<AuthBloc>().add(SignOutRequested());
-            },
-            icon: Icon(Icons.logout),
-          ),
-        ],
-      ),
-      body: Center(child: Text("Homepage")),
-    );
+    return Scaffold(body: Center(child: Text("Homepage")));
   }
 }
