0723596b5c0c959b21da213ba8bf41b55c67bef7
A Likith Reddy <alikithreddy381@gmail.com>
2026-06-09 06:58:29 +0530

    Fixed ios version issue and sharedpreferences issue

--- lib/config/dependency_injection/services/sharedpreferences_injection.dart
+++ lib/config/dependency_injection/services/sharedpreferences_injection.dart
@@ -2,7 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> initSharedPreferencesDependencies(GetIt sl) async {
-  sl.registerSingletonAsync<SharedPreferences>(
-    () async => await SharedPreferences.getInstance(),
-  );
+  final prefs = await SharedPreferences.getInstance();
+  sl.registerSingleton<SharedPreferences>(prefs);
 }
